a4842c1baa368fb32c1bda1b42b95cd99104a6b0
fadi123456 <fadiakrawi@gmail.com>
2017-12-19 15:08:59 +0100

    Interface

--- src/AccountAdmin.java
+++ src/AccountAdmin.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 public class AccountAdmin extends Account {
+    public AccountAdminToMarketBank checkmarket;
 
-    public AccountAdmin(ArrayList<Character> characters, ArrayList<AccountFriends> friends, String email, String password) {
+    public AccountAdmin(ArrayList<Character> characters, ArrayList<AccountFriends> friends, String email, String password, AccountAdminToMarketBank checkmarket) {
         super(characters, friends, email, password);
+        this.checkmarket = checkmarket;
     }
 }
 
--- src/CharTest.java
+++ src/CharTest.java
@@ -1,17 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CharTest {
 
     @Test
-    public static void CharTest(String[] args) {
+    public void CharTest() {
         CharHealer myhealer = new CharHealer("FAd", 1,1);
         myhealer.setSpeed(2000);
         //System.out.println(myhealer.getSpeed());
+        myhealer.setStrength(1000);
+
 
 
         assertEquals(myhealer.getSpeed(),2000);
+        assertEquals(myhealer.getStrength(),1000);
+        System.out.println(myhealer.getSpeed());
 
     }
 }
